60778d44cc3a5a8a13ffa86f538bd31ebe4336c2
Frank Kevin Zey <frank.kevin.zey@mni.thm.de>
2015-06-22 15:19:37 +0200

    implemented Mat3f class

---
+++ Math/Mat3f.cpp
@@ -0,0 +1,98 @@
+#include "Mat3f.hpp"
+
+#include <cstdio>
+#include <cstring>
+
+#include "Vec3f.hpp"
+
+static const unsigned int MATRIX_SIZE = sizeof( float ) * 9;
+
+/* ************************************************** */
+/* ************************************************** */
+/* **************** CON-/DESTRUCTORS  *************** */
+/* ************************************************** */
+/* ************************************************** */
+Mat3f::Mat3f( void )
+{
+  memset( m_Values, 0.0f, MATRIX_SIZE );
+}
+
+Mat3f::Mat3f( const Mat3f& other )
+{
+  memcpy( m_Values, other.m_Values, MATRIX_SIZE );
+}
+
+Mat3f::Mat3f( const Vec3f& row1, const Vec3f& row2, const Vec3f& row3 )
+{
+  m_Values[ 0 ][ 0 ] = row1.X(); m_Values[ 0 ][ 1 ] = row1.Y(); m_Values[ 0 ][ 2 ] = row1.Z();
+  m_Values[ 1 ][ 0 ] = row2.X(); m_Values[ 1 ][ 1 ] = row2.Y(); m_Values[ 1 ][ 2 ] = row2.Z();
+  m_Values[ 2 ][ 0 ] = row3.X(); m_Values[ 2 ][ 1 ] = row3.Y(); m_Values[ 2 ][ 2 ] = row3.Z();
+}
+
+Mat3f::~Mat3f( void ) { return; }
+
+/* ************************************************** */
+/* ************************************************** */
+/* ********************* PUBLIC  ******************** */
+/* ************************************************** */
+/* ************************************************** */
+
+Mat3f
+Mat3f::operator *( const Mat3f& factor ) const
+{
+  Mat3f result;
+
+  for ( unsigned int row = 0 ; row < 3 ; row++ )
+    for ( unsigned int col = 0 ; col < 3; col++ )
+      for( unsigned int trow = 0; trow < 3; trow++ )
+        result.m_Values[ row ][ col ] += m_Values[ trow ][ col ] * factor.m_Values[ row ][ trow ];
+
+  return result;
+}
+
+Mat3f&
+Mat3f::operator *=( const Mat3f& factor )
+{
+  Mat3f result;
+
+  for ( unsigned int row = 0 ; row < 3 ; row++ )
+    for ( unsigned int col = 0 ; col < 3; col++ )
+      for( unsigned int trow = 0; trow < 3; trow++ )
+        result.m_Values[ row ][ col ] += m_Values[ trow ][ col ] * factor.m_Values[ row ][ trow ];
+
+  memcpy( m_Values, result.m_Values, MATRIX_SIZE );
+
+  return *this;
+}
+
+float*
+Mat3f::operator []( unsigned int row )
+{
+  return m_Values[ row ];
+}
+
+const float*
+Mat3f::operator []( unsigned int row ) const
+{
+  return m_Values[ row ];
+}
+
+void
+Mat3f::Print( void ) const
+{
+  printf( "( %f | %f | %f )", m_Values[ 0 ][ 0 ], m_Values[ 0 ][ 1 ], m_Values[ 0 ][ 2 ] );
+  printf( "( %f | %f | %f )", m_Values[ 1 ][ 0 ], m_Values[ 1 ][ 1 ], m_Values[ 1 ][ 2 ] );
+  printf( "( %f | %f | %f )", m_Values[ 2 ][ 0 ], m_Values[ 2 ][ 1 ], m_Values[ 2 ][ 2 ] );
+}
+
+/* ************************************************** */
+/* ************************************************** */
+/* *******************  PROTECTED ******************* */
+/* ************************************************** */
+/* ************************************************** */
+
+/* ************************************************** */
+/* ************************************************** */
+/* ********************* PRIVATE ******************** */
+/* ************************************************** */
+/* ************************************************** */
---
+++ Math/Mat3f.hpp
@@ -0,0 +1,62 @@
+/*
+ This software is provided 'as-is', without any express or implied
+ warranty.  In no event will the authors be held liable for any damages
+ arising from the use of this software.
+
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+
+ 1. The origin of this software must not be misrepresented; you must not
+ claim that you wrote the original software. If you use this software
+ in a product, an acknowledgment in the product documentation would be
+ appreciated but is not required.
+ 2. Altered source versions must be plainly marked as such, and must not be
+ misrepresented as being the original software.
+ 3. This notice may not be removed or altered from any source distribution.
+
+ \file Mat3f.hpp
+
+ Created on: Jun 22, 2015
+ \author     fkzey
+ */
+
+#ifndef MATH_MAT3F_HPP_
+#define MATH_MAT3F_HPP_
+
+class Vec3f;
+
+/*
+ * GeekEngine::Mat3f
+ */
+class Mat3f
+{
+public:
+  Mat3f( void );
+  Mat3f( const Mat3f& );
+  Mat3f( const Vec3f&, const Vec3f&, const Vec3f& );
+  virtual
+  ~Mat3f( void );
+
+  Mat3f
+  operator *( const Mat3f& factor ) const;
+
+  Mat3f&
+  operator *=( const Mat3f& factor );
+
+  float*
+  operator []( unsigned int row );
+  const float*
+  operator []( unsigned int row ) const;
+
+
+  void
+  Print( void ) const;
+
+protected:
+private:
+  float m_Values[ 3 ][ 3 ];
+
+};
+
+#endif /* MATH_MAT3F_HPP_ */
